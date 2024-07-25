7e31230f49e63765fc7d5145d14266d6c0510ef1
Arush Nandankar <arush.nandankar@students.iiit.ac.in>
2024-07-25 21:44:43 +0530

    update SuffixArray.h

--- content/strings/SuffixArray.h
+++ content/strings/SuffixArray.h
@@ -18,25 +18,49 @@
 #pragma once
 
 struct SuffixArray {
-	vi sa, lcp;
-	SuffixArray(string& s, int lim=256) { // or basic_string<int>
-		int n = sz(s) + 1, k = 0, a, b;
-		vi x(all(s)+1), y(n), ws(max(n, lim)), rank(n);
-		sa = lcp = y, iota(all(sa), 0);
-		for (int j = 0, p = 0; p < n; j = max(1, j * 2), lim = p) {
-			p = j, iota(all(y), n - j);
-			rep(i,0,n) if (sa[i] >= j) y[p++] = sa[i] - j;
-			fill(all(ws), 0);
-			rep(i,0,n) ws[x[i]]++;
-			rep(i,1,lim) ws[i] += ws[i - 1];
-			for (int i = n; i--;) sa[--ws[x[y[i]]]] = y[i];
-			swap(x, y), p = 1, x[sa[0]] = 0;
-			rep(i,1,n) a = sa[i - 1], b = sa[i], x[b] =
-				(y[a] == y[b] && y[a + j] == y[b + j]) ? p - 1 : p++;
-		}
-		rep(i,1,n) rank[sa[i]] = i;
-		for (int i = 0, j; i < n - 1; lcp[rank[i++]] = k)
-			for (k && k--, j = sa[rank[i] - 1];
-					s[i + k] == s[j + k]; k++);
-	}
+    string ss;
+    vi sa, lcp;
+    SuffixArray(const string &s, int lim = 256) {
+        ss = s;
+        int n = s.size() + 1, k = 0, a, b;
+        vi x(all(s) + 1), y(n), ws(max(n, lim)), rank(n);
+        sa = lcp = y, iota(all(sa), 0);
+        for (int j = 0, p = 0; p < n; j = max(1, j * 2), lim = p) {
+            p = j, iota(all(y), n - j);
+            for (int i = 0; i < n; i++) if (sa[i] >= j) y[p++] = sa[i] - j;
+            fill(all(ws), 0);
+            for (int i = 0; i < n; i++) ws[x[i]]++;
+            for (int i = 1; i < lim; i++) ws[i] += ws[i - 1];
+            for (int i = n; i--;) sa[--ws[x[y[i]]]] = y[i];
+            swap(x, y), p = 1, x[sa[0]] = 0;
+            for (int i = 1; i < n; i++) a = sa[i - 1], b = sa[i], x[b] = (y[a] == y[b] && y[a + j] == y[b + j]) ? p - 1 : p++;
+        }
+        for (int i = 1; i < n; i++) rank[sa[i]] = i;
+        for (int i = 0, j; i < n - 1; lcp[rank[i++]] = k)
+        for (k && k--, j = sa[rank[i] - 1];
+        s[i + k] == s[j + k]; k++);
+    }
+    int lower_bound(const string &t) {
+        int l = 1, r = sa.size();
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (t <= ss.substr(sa[m], t.size())) r = m;
+            else l = m + 1;
+        }
+        return l;
+    }
+    int upper_bound(const string &t) {
+        int l = 1, r = sa.size();
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (t < ss.substr(sa[m], t.size())) r = m;
+            else l = m + 1;
+        }
+        return l;
+    }
+    bool is_substring(const string &t) {
+        int l = lower_bound(t);
+        int r = upper_bound(t);
+        return r - l > 0;
+    }
 };
